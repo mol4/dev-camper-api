5a11ea016cd91eaa2f27a95fe167ac234a367210
Ivan Malchanau <ivan.molchanoff@gmail.com>
2020-11-30 10:50:40 +0300

    morgan logger installation

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const logger = require('./middleware/logger');
+const morgan = require('morgan');
 
 //Route files
 const bootcamps = require('./routes/bootcamps');
@@ -10,10 +10,13 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-app.use(logger);
+//DEN logging middleware
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 //Mount routers
-
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
