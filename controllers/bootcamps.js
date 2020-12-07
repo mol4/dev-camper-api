6f27f41ce315bfcc53f44bf48b27eaa0d61f64a3
Ivan Malchanau <ivan.molchanoff@gmail.com>
2020-12-07 22:01:38 +0300

    get single and all bootcamps

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,17 +3,36 @@ const Bootcamp = require('../models/Bootcamp');
 //@desc Get all bootcamps
 //@route GET /api/v1/bootcamps
 //@access Public
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+
+    res.status(200).json({ success: true, data: bootcamps });
+  } catch (err) {
+    res.status(400).json({ success: false, errorMessage: err.message });
+  }
 };
 
 //@desc Get single bootcamp
 //@route GET /api/v1/bootcamps/:id
 //@access Public
-exports.getBootcamp = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+
+    if (!bootcamp) {
+      return res
+        .status(404)
+        .json({
+          success: false,
+          errorMessage: `Bottcamp ${req.params.id} not found`,
+        });
+    }
+
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (err) {
+    res.status(400).json({ success: false, errorMessage: err.message });
+  }
 };
 
 //@desc Create new bootcamp
