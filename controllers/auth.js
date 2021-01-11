fd4794238db5ae22e0959feb07fe4dff74f4b9ea
Ivan Malchanau <ivan.molchanoff@gmail.com>
2021-01-11 22:05:42 +0300

    Reset password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const sendEmail = require('../utils/sendEmail.js');
@@ -60,9 +61,9 @@ exports.getMe = asyncHandler(async (req, res, next) => {
   });
 });
 
-//@desc Get Curent logged in User
-//@route GET /api/v1/auth/forgotpassword
-//@access Private
+//@desc Forgot Password
+//@route POST /api/v1/auth/forgotpassword
+//@access Public
 exports.forgotPassword = asyncHandler(async (req, res, next) => {
   const user = await User.findOne({ email: req.body.email });
 
@@ -76,7 +77,7 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
   //Get reset url
   const resetUrl = `${req.protocol}//${req.get(
     'host'
-  )}/api/v1/resetpassword/${resetToken}`;
+  )}/api/v1/auth/resetpassword/${resetToken}`;
 
   const message = `You are receiving this email because you has requested the reset of a password. Please make a PUT request to \n\n ${resetUrl}`;
 
@@ -94,7 +95,7 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
     user.resetPasswordExpire = undefined;
 
     await user.save({ validateBeforeSave: false });
-    return next(new ErrorResponse('Email could not be saved'))
+    return next(new ErrorResponse('Email could not be saved'));
   }
 
   await user.save({ validateBeforeSave: false });
@@ -103,6 +104,34 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
     success: true,
     data: user,
   });
+});
+
+//@desc Reset Password
+//@route PUT /api/v1/auth/resetpassword/:resettoken
+//@access Public
+exports.resetPassword = asyncHandler(async (req, res, next) => {
+  //Get hashed token
+  const resetPasswordToken = crypto
+  .createHash('sha256')
+  .update(req.params.resettoken)
+  .digest('hex');
+
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    return next(new ErrorResponse('Invalid token', 400));
+  }
+
+  //Set new password
+  user.password = req.body.password;
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpire = undefined;
+
+  await user.save({validateBeforeSave: true});
+  sendTokenResponse(user, 200, res);
 });
 
 //Get token from model, create cookie and send response
--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const {
   login,
   getMe,
   forgotPassword,
+  resetPassword
 } = require('../controllers/auth');
 
 const router = express.Router();
@@ -11,7 +12,9 @@ const { protect } = require('../middleware/auth');
 
 router.post('/register', register);
 router.post('/login', login);
-router.post('/me', protect, getMe);
+router.get('/me', protect, getMe);
 router.post('/forgotpassword', forgotPassword);
+router.put('/resetpassword/:resettoken', resetPassword);
+
 
 module.exports = router;
