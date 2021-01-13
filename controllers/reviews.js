5ec439be78ec14252c621cefb9b598a747f0b598
Ivan Malchanau <ivan.molchanoff@gmail.com>
2021-01-13 13:43:03 +0300

    Get single review

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -8,15 +8,36 @@ const Bootcamp = require('../models/Bootcamp');
 //@route GET /api/v1/bootcamps/:bootcampId/reviews
 //@access Public
 exports.getReviews = asyncHandler(async (req, res, next) => {
-    if (req.params.bootcampId) {
-      const reviews = await Review.find({ bootcamp: req.params.bootcampId });
-  
-      res.status(200).json({
-        success: true,
-        count: reviews.length,
-        data: reviews,
-      });
-    } else {
-      res.status(200).json(res.advancedResults);
-    }
+  if (req.params.bootcampId) {
+    const reviews = await Review.find({ bootcamp: req.params.bootcampId });
+
+    res.status(200).json({
+      success: true,
+      count: reviews.length,
+      data: reviews,
+    });
+  } else {
+    res.status(200).json(res.advancedResults);
+  }
+});
+
+//@desc Get single review
+//@route GET /api/v1/reviews:id
+//@access Public
+exports.getReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findById(req.params.id).populate({
+    path: 'bootcamp',
+    select: 'name description',
   });
+
+  if (!review) {
+    return next(
+      new ErrorResponse(`No review found with id ${req.params.id}`, 404)
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+    data: review,
+  });
+});
